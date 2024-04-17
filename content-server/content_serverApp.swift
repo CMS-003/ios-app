449a52eafe5995b80c9324d4acce31e229157614
阮家友 <1439120442@qq.com>
2024-04-17 12:53:56 +0800

    Initial Commit

---
+++ content-server/content_serverApp.swift
@@ -0,0 +1,17 @@
+//
+//  content_serverApp.swift
+//  content-server
+//
+//  Created by jiayou on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct content_serverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
