60766c3b587bd2d535ed300cb491d0a753e03a37
阮家友 <1439120442@qq.com>
2024-04-29 10:54:02 +0800

    web set volume fail

--- content-server/ContentView.swift
+++ content-server/ContentView.swift
@@ -16,8 +16,7 @@ import AVFoundation
 import MediaPlayer
 
 struct WebViewContainer: UIViewRepresentable {
-  @Binding var url: String
-  @Binding var showVolume: Bool
+  @StateObject var global: Global
   @Binding var volume: Float
   @Binding var realVolume: Float
   let configuration: WKWebViewConfiguration
@@ -33,7 +32,7 @@ struct WebViewContainer: UIViewRepresentable {
     webview.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     
     // 加载 url
-    if let url = URL(string: url) {
+    if let url = URL(string: global.url) {
       let request = URLRequest(url: url);
       webview.load(request);
       webview.allowsBackForwardNavigationGestures = true
@@ -62,11 +61,10 @@ struct WebViewContainer: UIViewRepresentable {
       if message.name == "volumeChanged", let volumeInfo = message.body as? [String: Any], let volumeNow = volumeInfo["volume"] as? Double {
         // 在此处理收到的音量信息
         print("Received volume change: \(volumeNow)")
-        
-        self.parent.showVolume = true
+        self.parent.global.showVolumeTip = true
         self.parent.volume = Float(volumeNow)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-          self.parent.showVolume = false
+          self.parent.global.showVolumeTip = false
         }
       }
     }
@@ -77,10 +75,6 @@ struct WebViewContainer: UIViewRepresentable {
       self.parent = parent
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-      self.parent.url = webView.url?.absoluteString ?? ""
-      // 注入 JavaScript 代码以监听 SwiftUI 变量的变化
-    }
   }
 }
 
@@ -119,30 +113,26 @@ struct VolumeControlView: UIViewRepresentable {
 }
 
 struct ContentView: View {
-  @ObservedObject var store = Store()
+  @ObservedObject var global = Global()
   @StateObject var volumeObserver = Volumer()
-  // 入口 url
-  @State private var realURL: String = ""
+  
   // 静态文件服务器
   @State var Server = FileServer()
-  // 启动
-  @State var booted: Bool = false
+  
   // error
   @State var error = ""
   // 启动信息
   @State var message = ""
-  // 显示系统音量
-  @State var showVolume = false
-  @State private var volume:Float = AVAudioSession.sharedInstance().outputVolume
+  @State var volume = AVAudioSession.sharedInstance().outputVolume
+  
   let BaseURL = "http://ios.nat300.top"
   
   var body: some View {
     ZStack {
       VStack {
-        if booted {
+        if global.serverBooted {
           WebViewContainer(
-            url: $realURL,
-            showVolume: $showVolume,
+            global: global,
             volume: $volume,
             realVolume: $volumeObserver.volume,
             configuration: createConfiguration(volume: $volumeObserver.volume),
@@ -161,7 +151,6 @@ struct ContentView: View {
           ProgressView()
             .onAppear() {
               print("进入程序")
-              // print("volue: \(volumeObserver.volume)")
               message = "获取中..."
               shttp.get(BaseURL + "/gw/novel/v1/public/app-version/novel/latest")
                 .send { result in
@@ -175,7 +164,7 @@ struct ContentView: View {
                     }
                     // 版本判断
                     let app_version = (data["data"]["version"].string)!
-                    if app_version != store.app_version {
+                    if app_version != global.app_version {
                       message = "下载中.."
                       // 获取路径
                       let path = data["data"]["path"].string
@@ -187,23 +176,26 @@ struct ContentView: View {
                           error = "更新失败"
                           return
                         }
-                        print("更新应用: \(store.app_version) -> \(app_version)")
-                        store.app_version = app_version
+                        print("更新应用: \(global.app_version) -> \(app_version)")
+                        global.app_version = app_version
                         // 启动本地服务器
                         Server.start(dir: filedir!) {
-                          realURL = "http://127.0.0.1:\(Server.port)/novel/index.html"
-                          booted = true
+                          global.url = "http://127.0.0.1:\(Server.port)/novel/index.html"
+                          global.serverBooted = true
                         }
                       }
                     } else {
                       // 启动本地服务器
                       print("无需更新应用(\(app_version))")
-                      Server.start(dir: FileHelper().getDocumentDirectory()!) {
-                        realURL = "http://127.0.0.1:\(Server.port)/novel/index.html"
-                        booted = true
+                      DispatchQueue.global().async {
+                        Server.start(dir: global.documentDir) {
+                          DispatchQueue.main.async {
+                            global.url = "http://127.0.0.1:\(Server.port)/novel/index.html"
+                            global.serverBooted = true
+                          }
+                        }
                       }
                     }
-                    
                   case .failure(let err):
                     // 处理错误
                     print("Network request failed with error: \(err)")
@@ -213,21 +205,18 @@ struct ContentView: View {
             }
         }
       }
-      .onAppear {
-        
-      }
       
-      if showVolume {
+      if global.showVolumeTip {
         // MPVolumeView(frame: CGRect(x: -200, y: -200, width: 200, height: 50))
-        VolumeControlView(volume: $volume, show: $showVolume )
+        VolumeControlView(volume: $volume, show: $global.showVolumeTip )
           .frame(width: 200)
           .position(x: -200, y: -200)
           .allowsHitTesting(false)
         
         VStack {
           Text("音量: \(Int(volume*100))")
-            .padding(.vertical, 4)
-            .padding(.horizontal, 5)
+            .padding(.vertical, 8)
+            .padding(.horizontal, 15)
             .foregroundColor(.white)
             .background(Color.black.opacity(0.5))
             .cornerRadius(8)
---
+++ content-server/utils/Global.swift
@@ -0,0 +1,31 @@
+//
+//  Store.swift
+//  content-server
+//
+//  Created by jiayou on 2024/04/25.
+//
+
+import Foundation
+import AVFoundation
+
+// 简单保存数据
+class Global: ObservableObject {
+  // 本地服务器是否启动
+  @Published var serverBooted = false
+  // 本地初始访问地址
+  @Published var url = ""
+  // 文档目录
+  @Published var documentDir: URL
+  // app 版本(本地持久化)
+  @Published var app_version: String {
+    didSet {
+      UserDefaults.standard.set(app_version, forKey: "app_version")
+    }
+  }
+  @Published var showVolumeTip = false
+  // @Published var systemVolume = AVAudioSession.sharedInstance().outputVolume
+  init() {
+    self.documentDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    self.app_version = UserDefaults.standard.string(forKey: "app_version") ?? ""
+  }
+}
